b9a0011ea4c922f355c2d5c2cfd7277a337294e7
github username <github email address>
2018-04-25 20:13:21 -0400

    environment

--- config/environment.rb
+++ config/environment.rb
@@ -3,9 +3,17 @@ ENV['SINATRA_ENV'] ||= "development"
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
+configure :development do
 ActiveRecord::Base.establish_connection(
-    :adapter => "sqlite3",
-    :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
+  :adapter => "sqlite3",
+  :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
 )
+
+end
+
+
+configure :production do
+ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
+
 
 require_all 'app'
